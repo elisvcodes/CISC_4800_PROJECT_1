df90d946dcf917f6fdb9a4bdf40ac2bda21dd8c8
elisvcodes <sultanoveli@outlook.com>
2021-10-14 09:17:03 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: { enabled: true, content: ['./public/*.html'] },
+  darkMode: false,
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
